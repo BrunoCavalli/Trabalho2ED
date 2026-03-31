24ecffcfea2302d0698d44d5c4f62df055727316
unknown <brunocavalli12b@gmail.com>
2026-03-31 11:26:21 -0300

    criei estruturas iniciais

---
+++ OrderBook.hpp
@@ -0,0 +1,29 @@
+#include "Transaction.hpp"
+#include "Order.hpp"
+
+
+class OrderBook {
+
+private:
+
+    // Estruturas internas escolhidas pelos alunos
+    // para armazenar ordens de compra, venda e transações
+
+public:
+
+    OrderBook();
+    ~OrderBook();
+
+    bool submit(Order order);
+    bool cancel(int id);
+
+    Order* getBuyOrders(int* n);
+    Order* getSellOrders(int* n);
+    Transaction* getTransactions(int* n);
+
+    void printBuyOrders();
+    void printSellOrders();
+    void printTransactions();
+
+    // Outros métodos auxiliares, se necessário
+};
