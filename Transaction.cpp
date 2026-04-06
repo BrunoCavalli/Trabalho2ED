b7f1c6a1f2a1f3890089c25ebd9182dc8c8b4e7a
unknown <brunocavalli12b@gmail.com>
2026-04-06 09:39:13 -0300

    inicializei atributos do transaction

---
+++ Transaction.cpp
@@ -0,0 +1,12 @@
+#include "Transaction.hpp"
+#include "OrderBook.hpp"
+
+Transaction::Transaction(int buy_order_id, int sell_order_id, float execution_price){
+    this->buy_order_id = buy_order_id;
+    this->sell_order_id = sell_order_id;
+    this->execution_price = execution_price;
+}
+
+Transaction::~Transaction(){
+
+}
